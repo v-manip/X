1b8c69edfd975d5cd72b89282c9f4a9df4b5c867
Martin Hecher <martin.hecher@fraunhofer.at>
2014-06-24 10:32:24 +0200

    Updated documentation of the build-xtk.sh script

--- build-xtk.sh
+++ build-xtk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "\nSettings for the build can be changed in ./utils/_core/_builder.py\n"
 echo "\nThe output file is located at ./utils/xtk.js\n"
+echo "\nCopy the output file to the RectangularBoxViewer's root sub-directory 'deps/xtk.js' and call 'grunt' in the RBV root directory to install the updated RBV library to the Webclient library\n"
 python ./utils/build.py
